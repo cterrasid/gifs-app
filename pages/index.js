fa56fea674a799c3dddd09638b5518b2d72c8fd7
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-14 07:44:56 +0200

    Set the project in NextJs!

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+
+  return (
+    <section className="container">
+      <header>This is a header!</header>
+      <main>
+        This is a main!
+      </main>
+      <footer>This is a footer!</footer>
+    </section>
+  );
+}
