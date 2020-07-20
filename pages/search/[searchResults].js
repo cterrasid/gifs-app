805c3b3b0715c4f8fa5a1c807576f75fa92ee3d5
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-20 10:30:47 +0200

    Implement infinite Scroll using useCallback hook

--- pages/search/[searchResults].js
+++ pages/search/[searchResults].js
@@ -1,4 +1,4 @@
-import { Fragment, useRef, useEffect } from "react";
+import { Fragment, useRef, useEffect, useCallback } from "react";
 import ListOfGifs from "components/ListOfGifs";
 import useGifs from "hooks/useGifs";
 import useNearScreen from "hooks/useNearScreen";
@@ -6,31 +6,25 @@ import debounce from "just-debounce-it";
 
 export default function SearcResults({ keyword }) {
   const { isLoading, gifs, setPage } = useGifs({ keyword });
-  const externalRef = useRef();
+  const snitchRef = useRef();
   const { isNearScreen } = useNearScreen({
-    externalRef: isLoading ? null : externalRef,
+    externalRef: isLoading ? null : snitchRef,
     once: false
   });
 
-  const debounceHandleNextPage = useRef();
-  
-  console.log(isNearScreen);
-  // const handleNextPage = () => {
-  //   setPage(prevPage => prevPage + 1);
-  // };
-
-  const handleNextPage = () => {
-    console.log("next page");
-  };
-
-  debounceHandleNextPage.current = () =>
+  const debounceHandleNextPage = useCallback(
     debounce(() => {
-      console.log("next page"), 1000;
-    });
+      setPage(prevPage => prevPage + 1), 100;
+    }),
+    []
+  );
 
-  useEffect(() => {
-    if (isNearScreen) debounceHandleNextPage.current();
-  });
+  useEffect(
+    () => {
+      if (isNearScreen) debounceHandleNextPage();
+    },
+    [debounceHandleNextPage, isNearScreen]
+  );
 
   return (
     <Fragment>
@@ -41,10 +35,9 @@ export default function SearcResults({ keyword }) {
               {decodeURI(keyword)}
             </h3>
             <ListOfGifs gifs={gifs} />
-            <section id="snitch" ref={externalRef} />
+            <section id="snitch" ref={snitchRef} />
           </Fragment>}
       <br />
-      <button onClick={handleNextPage}>Get Next Page</button>
     </Fragment>
   );
 }
